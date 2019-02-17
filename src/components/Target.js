21c1d21b80668c5c066ce654c0e056ca119aa77f
majormalfunk <32271489+majormalfunk@users.noreply.github.com>
2019-02-17 17:08:48 +0200

    Some tweaks to the presentation of targets

--- src/components/Target.js
+++ src/components/Target.js
@@ -4,13 +4,19 @@ const Target = (props) => {
   if (props.target) {
     const target = props.target
     return (
-      <tr>
-        <td width="10%">Kohde:</td>
-        <td width="10%">{target.type}</td>
-        <td colSpan="2">{target.name}</td>
-        <td width="15%"><a href={target.hylyt_link}>hylyt.net({target.hylyt_id})</a></td>
-        <td width="15%"><a href={target.mj_link}>kyppi.fi({target.mj_id})</a></td>
-      </tr>
+      <>
+        <tr>
+          <td width="25%">Kohde: {target.type}</td>
+          <td width="45%" colSpan="3">{target.name}</td>
+          <td width="15%"><a href={target.hylyt_link}>hylyt.net({target.hylyt_id})</a></td>
+          <td width="15%"><a href={target.mj_link}>kyppi.fi({target.mj_id})</a></td>
+        </tr>
+        <tr>
+          <td width="15%">Sijainti: {target.latitude}, {target.longitude}</td>
+          <td width="10%">Syvyys: {target.depth}</td>
+          <td colSpan="4">&nbsp;</td>
+        </tr>
+      </>
     )
   } else {
     return null
